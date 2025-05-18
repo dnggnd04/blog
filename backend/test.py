3a6e22e004c6b1f483a2797ebb76e1f3e96fa8d6
TienDung <dnggnd04@gmail.com>
2025-05-19 01:21:31 +0700

    adđ dockerfile

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-print(os.path.join(BASE_DIR, 'app\.env'))
-load_dotenv(os.path.join(BASE_DIR, 'app\.env'))
-
-print(os.getenv("DATABASE_URL"))
